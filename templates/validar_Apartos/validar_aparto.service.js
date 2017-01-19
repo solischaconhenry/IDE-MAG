6b63626aecd120e9eb8cb2b943710ce0b1edc88b
hsolicha <hsolicha@hotmail.com>
2017-01-19 16:30:23 -0600

    correcciones en validaciones de apartos y errores generales

---
+++ templates/validar_Apartos/validar_aparto.service.js
@@ -0,0 +1,25 @@
+/**
+ * Created by usuario on 19/1/2017.
+ */
+angular.module('AppPrueba')
+    .service('ValidarService', ['$http','$q', function ($http, $q,InsertarFormularioFincaxF) {
+
+        this.getApartosAValidar = function (idfinca) {
+            var defered = $q.defer();
+            var promise = defered.promise;
+
+
+            $http.get('templates/validar_Apartos/validar_aparto.logic.php?action=apartoValidar&finca=' +idfinca)
+                .success(function(response) {
+                    defered.resolve(response);
+                });
+
+            return promise;
+        }
+
+
+
+
+
+    }]);
+
